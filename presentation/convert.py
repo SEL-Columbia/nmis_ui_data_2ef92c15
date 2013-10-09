8dc1f310b3ca3ca46056f960c66e26f823056047
Chris Tan <csytan@gmail.com>
2013-10-09 17:37:19 -0400

    Move lga sector data into separate file

--- presentation/convert.py
+++ presentation/convert.py
@@ -8,17 +8,29 @@ fname = os.path.join(cwd, 'summary_sectors.json')
 with open(fname, 'r') as f:
 	data = json.loads(f.read())
 
-data = data['relevant_data']['overview']
+overview_data = data['relevant_data']['overview']
 
-output = {
-	'overview': data['overview_and_map']['ids'],
-	'facility_overview': data['overview_facility_overview']['columns'],
-	'mdg_status': data['overview_mdg_status']
+lga_overview = {
+	'overview': overview_data['overview_and_map']['ids'],
+	'facility_overview': overview_data['overview_facility_overview']['columns'],
+	'mdg_status': overview_data['overview_mdg_status']
 }
 
 
-out = json.dumps(output, indent=4)
+out = json.dumps(lga_overview, indent=4)
 path = os.path.join(cwd, 'lga_overview.json')
 with open(path, 'w') as f:
 	f.write(out)
+
+
+sector_data = data['sectors']
+out = json.dumps(sector_data, indent=4)
+path = os.path.join(cwd, 'lga_sectors.json')
+with open(path, 'w') as f:
+	f.write(out)
+
+
+
+
+
 
