74a1931ad9704f701be834d1830895a9ddfce0f9
Chris Tan <csytan@gmail.com>
2013-10-16 16:42:31 -0400

    Include sectors.json indicators in indicators.json

--- variables/convert.py
+++ variables/convert.py
@@ -3,20 +3,32 @@ import os
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 fname = os.path.join(cwd, 'variables.json')
-
-
 with open(fname, 'r') as f:
 	data = json.loads(f.read())
 
-output = {}
+indicators = {}
 for indicator in data['list']:
-	output[indicator['slug']] = {
+	indicators[indicator['slug']] = {
 		'name': indicator['name'],
 		'description': indicator['description']
 	}
 
 
-out = json.dumps(output, indent=4)
+fname = os.path.join(cwd, 'sectors.json')
+with open(fname, 'r') as f:
+	data = json.loads(f.read())
+
+for sector in data['sectors']:
+	for column in sector['columns']:
+		slug = column['slug']
+		if slug not in indicators:
+			indicators[slug] = {
+				'name': column['name'],
+				'description': column.get('description', '')
+			}
+
+
+out = json.dumps(indicators, indent=4)
 path = os.path.join(cwd, 'indicators.json')
 with open(path, 'w') as f:
 	f.write(out)
